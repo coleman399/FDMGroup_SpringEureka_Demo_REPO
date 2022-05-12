8a025d8038dc674fc504911cdc9cee7aa1cf9a22
dillon.coleman <coleman399@gmail.com>
2022-05-12 14:12:12 -0700

    changed pom, v2 fully functional

--- messages_api_2/src/main/java/com/fdmgroup/messages_api_2/MessagesApi2Application.java
+++ messages_api_2/src/main/java/com/fdmgroup/messages_api_2/MessagesApi2Application.java
@@ -2,8 +2,10 @@ package com.fdmgroup.messages_api_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MessagesApi2Application {
 
 	public static void main(String[] args) {
--- messages_api_2/src/main/java/com/fdmgroup/messages_api_2/controller/MessageController.java
+++ messages_api_2/src/main/java/com/fdmgroup/messages_api_2/controller/MessageController.java
@@ -1,12 +1,14 @@
 package com.fdmgroup.messages_api_2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/message")
 public class MessageController {
     @GetMapping
     public String getMessage() {
-        return "Message API -2";
+        return "API-2 Message";
     }
 }
